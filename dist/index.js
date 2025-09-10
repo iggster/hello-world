93a2efc569eb1c3c3660aad6e5617f2e1d6edc0d
Ignacio Murillo <ignatius.murillo@gmail.com>
2025-09-09 21:23:37 -0400

    Add tax function

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
 "use strict";
 let mysize = 3;
 console.log(mysize);
+let x = 3;
+function calculateTax(income) {
+    if (income < 5000)
+        return income * 1.2;
+    return income * 1.4;
+}
+console.log(calculateTax(10000));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const enum Size {Small = 1, Medium = 2, Large = 3}
+const enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
 let mysize: Size = Size.Large;
 console.log(mysize);
+let x;
+
+// Note: the "taxYear?" means the paramter is optional.
+// Also the (taskYear || 2022) means if taxYear is undefined, use 2022 instead.
+function calculateTax(income: number, taxYear?: number): number {
+  if ((taxYear || 2022) < 2022) return income * 1.2;
+  return income * 1.4;
+}
+
+console.log(calculateTax(10000, 2022));
